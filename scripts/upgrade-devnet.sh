2898b3d8b6753d55c9f3f7c380cf531e798fd044
zkr99 <entercommercial888@gmail.com>
2026-07-30 20:52:06 +0200

    Add entros-verifier to the devnet upgrade script

--- scripts/upgrade-devnet.sh
+++ scripts/upgrade-devnet.sh
@@ -10,6 +10,9 @@ anchor build
 echo "Upgrading entros-anchor program..."
 solana program deploy -u devnet --program-id GZYwTp2ozeuRA5Gof9vs4ya961aANcJBdUzB7LN6q4b2 target/deploy/entros_anchor.so
 
+echo "Upgrading entros-verifier program..."
+solana program deploy -u devnet --program-id 4F97jNoxQzT2qRbkWpW3ztC3Nz2TtKj3rnKG8ExgnrfV target/deploy/entros_verifier.so
+
 echo "Upgrading entros-registry program..."
 solana program deploy -u devnet --program-id 6VBs3zr9KrfFPGd6j7aGBPQWwZa5tajVfA7HN6MMV9VW target/deploy/entros_registry.so
 
